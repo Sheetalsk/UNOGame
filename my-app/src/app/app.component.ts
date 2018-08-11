b841bb40f4925399ac4c8ea56f88ae8c9f2d758d
May <mymaymon@gmail.com>
2018-08-11 16:44:06 +0700

    a

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+import {MyRestService} from "./my-rest.service";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'My First Angular App';
+
+  constructor(private myRestService: MyRestService){}
+
+
+  ngOnInit() {
+    this.myRestService.getStatus().subscribe(status => {
+      this.title = status.title;
+    })
+  }
+}
